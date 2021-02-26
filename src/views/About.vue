38ddff4714580839cc2369416dc883a57e223c84
Achille Garin <achille.garin@viacesi.fr>
2021-02-26 11:19:49 +0100

    :speech_balloon::sparkles: About page finished and filled with text

--- src/views/About.vue
+++ src/views/About.vue
@@ -6,10 +6,10 @@
         <div class="even">
           <img :style="{ width: 150 - block.downsize + 'px' }" :alt="block.pictureAlt" :src="block.picture"/>
         </div>
-        <p class="even">{{ block.text }}</p>
+        <p v-html="block.text" class="even"></p>
       </div>
       <div v-else class="block">
-        <p class="odd">{{ block.text }}</p>
+        <p v-html="block.text" class="odd"></p>
         <div class="odd">
           <img :style="{ width: 150 - block.downsize + 'px' }" :alt="block.pictureAlt" :src="block.picture"/>
         </div>
@@ -60,6 +60,27 @@ p {
 p.odd {
     text-align: right;
 }
+@media all and (max-width: 1050px)
+{
+    .block {
+        flex-direction: column;
+        height: auto;
+    }
+    .block>div.odd, .block>div.even{
+        border: 0;
+        margin-bottom: 20px;
+    }
+    p, p.odd {
+        text-align: justify;
+        margin-bottom: 20px;
+    }
+    .block>div.odd {
+        order: -1;
+    }
+    a:hover {
+        text-decoration: underline;
+    }
+}
 </style>
 
 <script>
@@ -72,19 +93,19 @@ export default {
                     picture: require('../assets/images/bio/cesi.svg'),
                     pictureAlt: "Cesi logo",
                     downsize: 15,
-                    text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur."
+                    text: 'I am actually in my third year out of five at the <strong><a href="https://ecole-ingenieurs.cesi.fr/en/" target="blank">CESI</a></strong> school. I prepare here an engineer degree in computer science. This environment give me a lot of skills including: team work, autonomous capabilities and quick learning for main ones. My school use the <strong><a href="https://ecole-ingenieurs.cesi.fr/en/pedagogy-support/" target="blank">Problem Based Learning</a></strong> (PBL), it\'s a pro active method that is very close to real world jobs so I can be quickly available in an company project.'
                 },
                 {
                     picture: require('../assets/images/bio/code.svg'),
                     pictureAlt: "Code illustration logo",
                     downsize: 0,
-                    text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur."
+                    text: 'I had my first touch with developpment in 2014 as a Hobby and it quickly became a passion. Since there I have been going through many other languages by curiosity or for the purpose of my formation. Today <strong><a href="https://golang.org/" target="blank">Golang</a></strong> seems to me a really good competitor in the IT modern world. That why I choose it, simplicity and performance can resume the all language to me. For Vue.js there is no debate it is just the best in its field.'
                 },
                 {
                     picture: require('../assets/images/bio/gnu.svg'),
                     pictureAlt: "GNU head logo",
                     downsize: 0,
-                    text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur."
+                    text: 'The <strong><a href="https://fsf.org/" target="blank">Free Software</a></strong> movement is something that is really close to my heart. Since 2016 I use GNU/Linux operating systems as a daily driver on my main computer. I know this type of OS like the back of my hand. It is handy to know how to use the GO TO system for devops and system administration ;-)'
                 }
             ]
         }
